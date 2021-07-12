7e94bfdc7b38df602814891e965f99e263f4ca8e
DESKTOP-TTLUSJ2\baoki <baokiinkk@gmail.com>
2021-07-11 20:03:33 -0700

    feat(dev): edit host address
    thay đổi host

--- app/src/main/java/com/example/rollcall/di/ApiModule.kt
+++ app/src/main/java/com/example/rollcall/di/ApiModule.kt
@@ -1,6 +1,7 @@
 package com.example.rollcall.di
 
 import com.example.rollcall.data.api.ApiService
+import com.example.rollcall.data.respository.Repository
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
@@ -10,18 +11,26 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object  ApiModule {
+object ApiModule {
     @Provides
     @Singleton
     fun provideUseApi(retrofit: Retrofit): ApiService {
         return retrofit.create(ApiService::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
 
     @Provides
     @Singleton
@@ -29,23 +38,22 @@ object  ApiModule {
         return GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create()
     }
 
-    @Provides
     @Singleton
-    fun provideHttpClient(): OkHttpClient {
-        val okHttpClientBuilder = OkHttpClient.Builder()
-
-        return okHttpClientBuilder.build()
-    }
+    @Provides
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
 
     @Provides
     @Singleton
     fun provideRetrofit(factory: Gson, client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("http://tncnhan.codes/api/")
+            .baseUrl("https://tncnhan.codes/api/")
             .addConverterFactory(GsonConverterFactory.create(factory))
             .client(client)
             .build()
     }
-
 
 }
